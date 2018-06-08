83c67ea9cbb0b45db96060efef8a22bc1cdf5e36
Monster <948807264@qq.com>
2018-06-08 22:17:17 +0800

    Update 988C.cpp

--- 988C.cpp
+++ 988C.cpp
@@ -1,1 +1,27 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int a[200005];
+typedef pair<int,int>pir;
+map<int,pir>mp;
+int main()
+{
+    int k,n;
+    cin>>k;
+    for(int i=1;i<=k;i++){
+        cin>>n;
+        int sum=0;
+        for(int j=1;j<=n;j++){
+            cin>>a[j];
+            sum+=a[j];
+        }
+        for(int j=1;j<=n;j++){
+            pir tmp=mp[sum-a[j]];
+
+            if(tmp==make_pair(0,0)) mp[sum-a[j]]=make_pair(i,j);//cout<<tmp.first<<" true"<<tmp.second<<endl;
+            else if(tmp.first!=i) {cout<<"YES\n"<<tmp.first<<" "<<tmp.second<<"\n"<<i<<" "<<j<<endl;return 0;}
+        }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}
